39d948269764b8f92e068424726350454f73ebd6
Anbar Saleem <anbar.saleem@gmail.com>
2021-05-02 06:00:37 -0400

    Completed Day 3 part 2

--- advent-of-code/day-3/treeEncounterCounter.js
+++ advent-of-code/day-3/treeEncounterCounter.js
@@ -1,26 +1,32 @@
 //Reads the input file
-var fs = require("fs");
-var text = fs.readFileSync("input3.txt", "utf-8");
+const fs = require("fs");
+const text = fs.readFileSync("input3.txt", "utf-8");
 
-var textByLine = text.trim().split('\n');
+const textByLine = text.trim().split('\n');
 
-var numTreesEncountered = 0;
-var x = 0;
-var y = 0;
-var lineLastIndex = textByLine[0].length - 1;
+var numTreesEncountered = (run, rise) => {
+    var count = 0;
+    var x = 0;
+    var y = 0;
+    var lineLastIndex = textByLine[0].length - 1;
 
-while(y < textByLine.length - 1) {
-    ///Conditional to check if you need to loop back to the beginning of the line if you are near the end of the row
-    if((lineLastIndex - x) < 3) {
-        x = x - lineLastIndex - 1;
+    while(y < textByLine.length - 1) {
+        ///Conditional to check if you need to loop back to the beginning of the line if you are near the end of the row
+        if((lineLastIndex - x) < run) {
+            x = x - lineLastIndex - 1;
+        }
+        x += run;
+        y += rise;
+
+        if(textByLine[y][x] === "#") {
+            count++;
+        }
     }
-    x += 3;
-    y++;
+    return count;
+};
 
-    if(textByLine[y][x] === "#") {
-        numTreesEncountered++;
-    }
-}
 
-console.log("Number of Trees Encountered on Slope: " + numTreesEncountered);
+console.log("Number of Trees Encountered on Slope: " + numTreesEncountered(3, 1)); //Part 1 Answer
 
+const partTwoAnswer = numTreesEncountered(1, 1) * numTreesEncountered(3, 1) * numTreesEncountered(5, 1) * numTreesEncountered(7, 1) * numTreesEncountered(1, 2);
+console.log("Part Two Answer: " + partTwoAnswer);
